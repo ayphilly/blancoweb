555b1bba3a78fe614dad16e4cf3ebceacc3011de
adewale ayomiposi <aphilemon.aa@gmail.com>
2020-12-26 17:41:46 +0100

    added loading svg to the shop page

--- src/pages/shopProduct/shop.js
+++ src/pages/shopProduct/shop.js
@@ -5,7 +5,7 @@ import ReactPaginate from 'react-paginate';
 import Posts from '../../component/posts'
 import Sidebar from './shop_sidebar'
 import './shop.css'
-
+import loading1 from "../../svg/loading1.svg"
 export default class Shop extends Component {
     
     constructor(props) {
@@ -15,7 +15,8 @@ export default class Shop extends Component {
             data: [],
             perPage: 20,
             currentPage: 0,
-            post:[]
+            post:[],
+            available:false
             
         };
         this.handlePageClick = this
@@ -41,7 +42,8 @@ export default class Shop extends Component {
                 this.setState({
                     pageCount: Math.ceil(data.length / this.state.perPage),
                     post,
-                    postData
+                    postData,
+                    available: true
                 })
             });
     }
@@ -63,56 +65,70 @@ export default class Shop extends Component {
         
    
     render() {
-        return (
-            <div className="shopy">
-                <div className="bar">
-                    <Sidebar/>
+        var x = 0;
+        if (!this.state.available) {
+            return (
+                <div className ="load-container">
+                    <div className="load-section">
+                        <img src={loading1}/> 
+                        <p> loading ...</p>
+                    </div> 
                 </div>
-                <div className="shop-product">
-                    <div className="select">
-                        <select name="colors" id="colors">
-                        <option value="volvo">Color</option>
-                            <option value="Red">Red</option>
-                            <option value="white">White</option>
-                            <option value="black">Black</option>
-                            <option value="pink">Pink</option>                            
-                        </select>
-
-                        <select name="sortby" id="sortby">
-                            <option value="price">price</option>
-                            <option value="hl">highest-lowest</option>
-                            <option value="lh">lowest-highest</option>
-                            <option value="audi">Audi</option>
-                        </select>
-
-                        <select name="cars" id="cars">
-                            <option value="volvo">Volvo</option>
-                            <option value="saab">Saab</option>
-                            <option value="mercedes">Mercedes</option>
-                            <option value="audi">Audi</option>
-                        </select>
-
+            )
+        } else {
+            return (
+                <div className="shopy">
+                    <div className="bar">
+                        <Sidebar/>
                     </div>
-                    <div className="prod">
-                        {this.state.postData}
+                    <div className="shop-product">
+                        <div className="select">
+                            <select name="colors" id="colors">
+                            <option value="volvo">Color</option>
+                                <option value="Red">Red</option>
+                                <option value="white">White</option>
+                                <option value="black">Black</option>
+                                <option value="pink">Pink</option>                            
+                            </select>
+    
+                            <select name="sortby" id="sortby">
+                                <option value="price">price</option>
+                                <option value="hl">highest-lowest</option>
+                                <option value="lh">lowest-highest</option>
+                                <option value="audi">Audi</option>
+                            </select>
+    
+                            <select name="cars" id="cars">
+                                <option value="volvo">Volvo</option>
+                                <option value="saab">Saab</option>
+                                <option value="mercedes">Mercedes</option>
+                                <option value="audi">Audi</option>
+                            </select>
+    
+                        </div>
+                        <div className="prod">
+                            {this.state.postData}
+                        </div>
+                        <ReactPaginate
+                            previousLabel={"prev"}
+                            nextLabel={"next"}
+                            breakLabel={"..."}
+                            breakClassName={"break-me"}
+                            pageCount={this.state.pageCount}
+                            marginPagesDisplayed={2}
+                            pageRangeDisplayed={5}
+                            onPageChange={this.handlePageClick}
+                            containerClassName={"pagination"}
+                            subContainerClassName={"pages pagination"}
+                            activeClassName={"active"}
+                            className="pagi"
+                        />
                     </div>
-                    <ReactPaginate
-                        previousLabel={"prev"}
-                        nextLabel={"next"}
-                        breakLabel={"..."}
-                        breakClassName={"break-me"}
-                        pageCount={this.state.pageCount}
-                        marginPagesDisplayed={2}
-                        pageRangeDisplayed={5}
-                        onPageChange={this.handlePageClick}
-                        containerClassName={"pagination"}
-                        subContainerClassName={"pages pagination"}
-                        activeClassName={"active"}
-                        className="pagi"
-                    />
                 </div>
-            </div>
+    
+            )
 
-        )
+        }
+       
     }
 }
