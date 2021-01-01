e56615ff22ef817f2b802b230c42d21f70faef19
adewale ayomiposi <aphilemon.aa@gmail.com>
2021-01-01 19:28:25 +0100

    added some regex for the register page

--- src/component/login/register.js
+++ src/component/login/register.js
@@ -12,7 +12,23 @@ export class Register extends Component {
         
     }
     state = {
-        reflink: ''
+        reflink: '',
+        firstname: '',
+        lastname : '',
+        number : '', 
+        email : '',
+        password : '',
+        formErrors: {
+            email: '',
+            firstname : '',
+            lastname : '',
+            number : '',
+            password: ''
+        },
+        emailValid: false,
+        passwordValid: false,
+        nameValid : false,
+        formValid: false
 
     }
     reveal = ()=> {
@@ -23,6 +39,67 @@ export class Register extends Component {
         }else {
             this.setState({ reflink : false  });
         }
+
+    }
+    validateField = (fieldName, value) => {
+        
+        let fieldValidationErrors = this.state.formErrors;
+        let emailValid = this.state.emailValid;
+        let passwordValid = this.state.passwordValid;
+        let nameValid = this.state.nameValid;
+        let numberValid = this.state.numberValid;
+        let lastnameValid = false;
+        let firstnameValid = false;
+
+        switch(fieldName) {
+            case 'firstname':
+                firstnameValid = value.match(/^[a-zA-Z ]+$/);
+                fieldValidationErrors.fieldName = firstnameValid ? '' : 'firstname not valid'
+                break;
+            case 'lastname':
+                lastnameValid = value.match(/^[a-zA-Z ]+$/);
+                fieldValidationErrors.fieldName = lastnameValid ? '' : 'lastname not valid'
+                break;
+            case 'email':
+                emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+                fieldValidationErrors.fieldName = emailValid ? '' : 'email is not valid'
+                break;
+            case 'password':
+                passwordValid = value.length >= 6;
+                fieldValidationErrors.fieldName = passwordValid ? '': ' is too short';
+                break;
+            case 'number' :
+                // numberValid : value.match(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im);
+                numberValid = value.isEmpty();
+                fieldValidationErrors.fieldName = numberValid ? '' : 'email should not be empty'
+                break;
+            default:
+                
+                break;
+        }
+        this.setState({
+            formErrors : fieldValidationErrors,
+            emailValid: emailValid,
+            passwordValid: passwordValid,
+            nameValid : firstnameValid && lastnameValid,
+            numberValid: numberValid
+
+        }, this.validateForm)
+
+    }
+    validateForm = () => {
+        this.setState({
+            formValid : this.state.emailValid && this.state.passwordValid && this.state.nameValid && this.state.numberValid
+        })
+    }
+
+    handleUserInput = (e)=> {
+        var name = e.target.value;
+        var value = e.target.value;
+        
+        this.setState({
+            [name]: value
+        }, this.validateField(name, value))
 
     }
     
@@ -44,11 +121,11 @@ export class Register extends Component {
                     <div className="names">
                         <div className="first-name">
                             <label for="inputFirstName">FIRST NAME</label>
-                            <input type="text" className="fname" id="fname" name="fname" placeholder="First Name"></input>
+                            <input type="text" className="fname" id="fname" name="firstname" placeholder="First Name"></input>
                         </div>
                         <div className="last-name">
                             <label for="inputLastName">LAST NAME</label>
-                            <input type="text" className="lname" name="lname" id="lname" placeholder="Last Name"></input>
+                            <input type="text" className="lname" name="lastname" id="lname" placeholder="Last Name"></input>
                         </div>
                         
                     </div>
@@ -59,7 +136,7 @@ export class Register extends Component {
                         </div>
                         <div className="phone-number">
                             <label for="number">PHONE NUMBER</label>
-                            <input type="text" className="number" id="pnumber" placeholder="+234"></input>
+                            <input type="text" className="number" id="pnumber" name="number" placeholder="+234"></input>
                         </div>         
                     </div>
                     <div className="password-input">
@@ -67,10 +144,10 @@ export class Register extends Component {
                             <label for="password">PASSWORD</label>
                             <input type="text" className="password" name="password" id="pword" placeholder="Password"></input>
                         </div>
-                        <div className="pword">
+                        {/* <div className="pword">
                             <label for="password">CONFIRM PASSWORD</label>
                             <input type="text" className="password" name="cpassword" id="pword" placeholder="Password"></input>
-                        </div>                             
+                        </div>                              */}
                     </div>
                     <div className="bottom">
                         <div className="c-box">
