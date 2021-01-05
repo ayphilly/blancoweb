1807b90d1d8da3d1041ada17445761cadfc22554
mac <mac@macs-MacBook-Pro.local>
2021-01-05 17:05:32 +0100

    improved register experience

--- src/component/login/register.js
+++ src/component/login/register.js
@@ -27,7 +27,8 @@ export class Register extends Component {
         },
         emailValid: false,
         passwordValid: false,
-        nameValid : false,
+        firstnameValid : false,
+        lastnameValid: false,
         formValid: false
 
     }
@@ -46,42 +47,46 @@ export class Register extends Component {
         let fieldValidationErrors = this.state.formErrors;
         let emailValid = this.state.emailValid;
         let passwordValid = this.state.passwordValid;
-        let nameValid = this.state.nameValid;
+        
         let numberValid = this.state.numberValid;
-        let lastnameValid = false;
-        let firstnameValid = false;
+        let lastnameValid = this.state.firstnameValid;
+        let firstnameValid = this.state.lastnameValid;
 
         switch(fieldName) {
             case 'firstname':
-                firstnameValid = value.match(/^[a-zA-Z ]+$/);
-                fieldValidationErrors.fieldName = firstnameValid ? '' : 'firstname not valid'
+                var patt = new RegExp(/^[a-zA-Z ]+$/)
+                firstnameValid = patt.test(value);
+                fieldValidationErrors.firstname = firstnameValid ? '' : 'firstname not valid'
                 break;
             case 'lastname':
-                lastnameValid = value.match(/^[a-zA-Z ]+$/);
-                fieldValidationErrors.fieldName = lastnameValid ? '' : 'lastname not valid'
+                var patt = new RegExp(/^[a-zA-Z ]+$/)
+                lastnameValid = patt.test(value);
+                fieldValidationErrors.lastname = lastnameValid ? '' : 'lastname not valid'
                 break;
             case 'email':
-                emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
-                fieldValidationErrors.fieldName = emailValid ? '' : 'email is not valid'
+                
+                var patt = new RegExp(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i)
+                emailValid = patt.test(value);
+                fieldValidationErrors.email = emailValid ? '' : ' is invalid';
                 break;
             case 'password':
                 passwordValid = value.length >= 6;
-                fieldValidationErrors.fieldName = passwordValid ? '': ' is too short';
+                fieldValidationErrors.password = passwordValid ? '': ' is too short';
                 break;
             case 'number' :
                 // numberValid : value.match(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im);
-                numberValid = value.isEmpty();
-                fieldValidationErrors.fieldName = numberValid ? '' : 'email should not be empty'
+                numberValid = value.length != 0;
+                fieldValidationErrors.number = numberValid ? '' : 'number should not be empty'
                 break;
             default:
-                
                 break;
         }
         this.setState({
             formErrors : fieldValidationErrors,
             emailValid: emailValid,
             passwordValid: passwordValid,
-            nameValid : firstnameValid && lastnameValid,
+            firstnameValid : firstnameValid,
+            lastnameValid : lastnameValid,
             numberValid: numberValid
 
         }, this.validateForm)
@@ -89,17 +94,23 @@ export class Register extends Component {
     }
     validateForm = () => {
         this.setState({
-            formValid : this.state.emailValid && this.state.passwordValid && this.state.nameValid && this.state.numberValid
+            formValid : this.state.emailValid && this.state.passwordValid && this.state.firstnameValid && this.state.lastnameValid && this.state.numberValid
         })
     }
+    handleSubmit(event) {
+        event.preventDefault();
+        alert(`Name submiited: ${JSON.stringify(this.state)}`);
+        
+      }
 
     handleUserInput = (e)=> {
-        var name = e.target.value;
+        
+        var name = e.target.name;
         var value = e.target.value;
         
         this.setState({
             [name]: value
-        }, this.validateField(name, value))
+        }, () => { this.validateField(name, value) })
 
     }
     
@@ -113,7 +124,7 @@ export class Register extends Component {
             <div className="container-fluid wow fadeInLeft reg">
                 <div className="register-form">
                 
-                <form>
+                <form onSubmit={(e) => this.handleSubmit(e)}>
                     <h3>
                         Welcome, <br/>
                         Create  your Blanco account 
@@ -121,28 +132,33 @@ export class Register extends Component {
                     <div className="names">
                         <div className="first-name">
                             <label for="inputFirstName">FIRST NAME</label>
-                            <input type="text" className="fname" id="fname" name="firstname" placeholder="First Name"></input>
+                            <input type="text" className="fname" id="fname" name="firstname" placeholder="First Name" onChange={(event) => this.handleUserInput(event)}></input>
+                            { this.state.formErrors.firstname && <p className="formerror"> {this.state.formErrors.firstname} </p> }
                         </div>
                         <div className="last-name">
                             <label for="inputLastName">LAST NAME</label>
-                            <input type="text" className="lname" name="lastname" id="lname" placeholder="Last Name"></input>
+                            <input type="text" className="lname" name="lastname" id="lname" placeholder="Last Name" onChange={(event) => this.handleUserInput(event)}></input>
+                            { this.state.formErrors.lasttname && <p className="formerror"> {this.state.formErrors.firstname} </p> }
                         </div>
                         
                     </div>
                     <div className="contact">
                         <div className="mail">
                             <label for="Email">E-MAIL</label>
-                            <input type="text" className="email" name="email" id="Email" placeholder="Email"></input>
+                            <input type="text" className="email" name="email" id="Email" placeholder="Email" onChange={(event) => this.handleUserInput(event)}></input>
+                            { this.state.formErrors.email && <p className="formerror"> {this.state.formErrors.email} </p> }
                         </div>
                         <div className="phone-number">
                             <label for="number">PHONE NUMBER</label>
-                            <input type="text" className="number" id="pnumber" name="number" placeholder="+234"></input>
+                            <input type="text" className="number" id="pnumber" name="number" placeholder="+234" onChange={(event) => this.handleUserInput(event)}></input>
+                            { this.state.formErrors.number && <p className="formerror"> {this.state.formErrors.number} </p> }
                         </div>         
                     </div>
                     <div className="password-input">
                         <div className="pword">
                             <label for="password">PASSWORD</label>
-                            <input type="text" className="password" name="password" id="pword" placeholder="Password"></input>
+                            <input type="password" className="password" name="password" id="pword" placeholder="Password" onChange={(event) => this.handleUserInput(event)}></input>
+                            { this.state.formErrors.password && <p className="formerror"> {this.state.formErrors.password} </p> }
                         </div>
                         {/* <div className="pword">
                             <label for="password">CONFIRM PASSWORD</label>
@@ -153,7 +169,7 @@ export class Register extends Component {
                         <div className="c-box">
                         <span className="checkmark"><input type="checkbox"></input> SUBSCRIBE </span>
                         </div>
-                        <button type="submit" className="button-create">CREATE</button>
+                        <button type="submit" className="button-create" >CREATE</button>
                         <div className="login-link">
                            { this.state.reflink ? <a href="#" onClick={this.props.Close}>Already have an account ? Login here</a>
                               : <Link className="linkk" to="/login" style={{textDecoration:'none'}}>Already have an account ? Login here </Link>
